44b1aad19d6e90b7d0600b0f1345cf0203f494af
Martin <martuanez@gmail.com>
2012-06-21 17:58:00 -0300

    Added administration controller and actions
    Also added Authorize attribute for this controller

---
+++ Bios User Management Tool/Controllers/administracionController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bios_User_Management_Tool.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class administracionController : Controller
+    {
+        //
+        // GET: /administracion/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Alumnos()
+        {
+            return View();
+        }
+
+        public ActionResult Alumnos(int Id)
+        {
+            return View();
+        }
+
+        public ActionResult Docentes()
+        {
+            return View();
+        }
+
+        public ActionResult Docentes(int Id)
+        {
+            return View();
+        }
+        
+        public ActionResult Cursos()
+        {
+            return View();
+        }
+
+        public ActionResult Cursos(int Id)
+        {
+            return View();
+        }
+    }
+}
